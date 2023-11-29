5d804342c6ad85a28e51a8e7edd7611271640f82
ziiyouth <ziy913@naver.com>
2023-11-29 17:40:54 +0900

    first commit

---
+++ goormthon/src/main/java/org/goorm/goormthon/GoormthonApplication.java
@@ -0,0 +1,13 @@
+package org.goorm.goormthon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoormthonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoormthonApplication.class, args);
+	}
+
+}
---
+++ goormthon/src/test/java/org/goorm/goormthon/GoormthonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.goorm.goormthon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoormthonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
