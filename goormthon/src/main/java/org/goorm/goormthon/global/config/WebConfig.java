68b17a0c09061691d0729359b2074f94e8bbf40d
ziiyouth <ziy913@naver.com>
2023-11-29 22:10:30 +0900

    [feat] CORS 설정

--- goormthon/src/main/java/org/goorm/goormthon/global/config/WebConfig.java
+++ goormthon/src/main/java/org/goorm/goormthon/global/config/WebConfig.java
@@ -9,16 +9,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        // 모든 경로에 앞으로 만들 모든 CORS 정보를 적용한다
         registry.addMapping("/**")
-                // Header의 Origin에 들어있는 주소가 http://localhost:3000인 경우를 허용한다
-                .allowedOrigins("http://localhost:3000")
-                // 모든 HTTP Method를 허용한다.
-                .allowedMethods("*")
-                // HTTP 요청의 Header에 어떤 값이든 들어갈 수 있도록 허용한다.
-                .allowedHeaders("*")
-                // 자격증명 사용을 허용한다.
-                // 해당 옵션 사용시 allowedOrigins를 * (전체)로 설정할 수 없다.
-                .allowCredentials(true);
+                .allowedOrigins("http://localhost:8080", "http://localhost:8081", "http://localhost:3000", "http://localhost:3001", "http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true)
+                .maxAge(3000);
     }
 }
