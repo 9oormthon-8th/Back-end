ba3f7cb305e0057e9b96bf52e648d9f6a49ae6c6
ziiyouth <ziy913@naver.com>
2023-11-29 22:16:49 +0900

    [feat] CORS 설정

--- goormthon/src/main/java/org/goorm/goormthon/global/config/WebConfig.java
+++ goormthon/src/main/java/org/goorm/goormthon/global/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:8080", "http://localhost:8081", "http://localhost:3000", "http://localhost:3001", "http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("*")
                 .allowCredentials(true)
                 .maxAge(3000);
     }
